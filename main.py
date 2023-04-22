f32b613d6cb58ac6b0df0823a58271d30b6cb2f5
Maurice Okumu <maurice@Maurices-MacBook-Pro.local>
2023-04-22 12:56:25 -0500

    Add main bricks layout to game

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import BreakingBricks
 
 
 def print_hi(name):
@@ -11,6 +8,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    BreakingBricks.break_bricks()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
